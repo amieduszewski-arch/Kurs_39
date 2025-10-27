f5f7f35e48a88e3a504ac9acf1323fbe54fbb830
Aleksander <sasha.medush@MacBookAir.lan>
2025-10-27 15:11:40 +0100

    first commit

---
+++ zyczenia.py
@@ -0,0 +1,16 @@
+import datetime
+
+print("hello wordl")
+
+Imie = input("Podaj imię: ")
+Rok_urodzenia = int(input("Podaj rok urodzenia: "))
+Wiadomosc = input("Napisz swoje zyczenia: ")
+Imie_nadacy = input("Podaj imie nadacy: ")
+
+obecny_rok = datetime.datetime.now().year
+wiek = obecny_rok - Rok_urodzenia
+
+print(f"{Imie}, wszystkiego najlepszego z okazji {wiek} urodzin!")
+print(f"{Wiadomosc}")
+print(f"{Imie_nadacy}")
+
