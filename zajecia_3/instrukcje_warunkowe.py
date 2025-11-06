a40dc6f693f3a0266ac25637fb3720b24f2e3178
sasha.medush <a.mieduszewski@gmail.com>
2025-11-06 22:08:16 +0100

    zmiana w w zadaniu 5

---
+++ zajecia_3/instrukcje_warunkowe.py
@@ -0,0 +1,91 @@
+# print("hello world")
+# imie = "Aleksander"
+from platform import win32_edition
+
+# print("Witam w zabce")
+#
+# nazwa_produktu = input("Podaj nazwe produktu: ")
+# cena_produktu = float(input("Podaj cene produktu: "))
+#
+# if nazwa_produktu == "papierosy" or nazwa_produktu == "piwo" or nazwa_produktu == "energetyk" or nazwa_produktu == "alkohol":
+#     wiek = int(input("Podaj swoj wiek (jest wymagany): "))
+#     if wiek >= 18:
+#         print(f"Zakupiles {nazwa_produktu} w cenie {cena_produktu}. Masz {wiek} lat")
+#     else:
+#         print(f"Niestety nie mozez kupic {nazwa_produktu} poniewaz nie masz 18 lat. ")
+#
+# else:
+#     print(f"Zakupiles {nazwa_produktu} w cenie {cena_produktu}. ")
+#
+# print("Zegnamy do zobaczenia w zabce!")
+
+#zadanie_2
+
+# zawod = input("Podaj zawod w ktorym pracujesz?: ")
+# wiek = int(input("Podaj swoj wiek?: "))
+#
+# if zawod == "policja":
+#     print(f"Do emeretury zostalo ci {50 - wiek} lat.")
+# elif zawod == "zolnierz":
+#     print(f"Do emeretury zostalo ci {45 - wiek} lat.")
+# elif zawod == "nauczyciel":
+#     print(f"Do emerytury zostalo ci {65 - wiek} lat.")
+# elif zawod == "programista":
+#     print(f"Do emerytury zostalo ci {65 - wiek} lat.")
+# else:
+#     print("Niestety nie znam twojego zawodu")
+
+# petle white
+
+# wiek = int(input("Podaj swoj wiek?: "))
+#
+# while wiek < 66:
+#     if wiek >= 65:
+#         print(f"Powinienes byc na emereturzy po ciezkiej pracy!: ")
+#         break
+#     elif wiek == 18:
+#         print(f"Masz 18 lat, nie mysl o emereturze, a tylko o prace")
+#         break
+#     else:
+#         print(f"Nie masz jeszcze 65 lat, twoj wiek {wiek} musisz jeszcze dopracowac {65 - wiek} lat!")
+#     wiek += 1
+
+#Pietle FOR
+
+# ilosc_ocen = int(input("Podaj ilosc ocen do sredniu: "))
+# suma_ocen = 0
+#
+# obecna_ocena = 0
+#
+# while obecna_ocena < ilosc_ocen:
+#     obecna_ocena += 1
+#     ocena = int(input("Podaj ocene?: "))
+#     suma_ocen += ocena
+# print(f"Srednia aryfmetyczna z ocen to: {suma_ocen/ilosc_ocen}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
