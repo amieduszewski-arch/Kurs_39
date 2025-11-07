d943282ac3df382d8661fb6fddfc4031eafa26bf
sasha.medush <a.mieduszewski@gmail.com>
2025-11-07 21:42:05 +0100

    update

---
+++ Zajecie_3_warsztaty/student.py
@@ -0,0 +1,60 @@
+# Napisz program, który policzy statystyki dotyczące ocen studentów.
+#
+# Program działa tak:
+#   1) Pyta użytkownika: "Ile ocen chcesz podać?" gotowe
+#   2) Wczytuje kolejno oceny (każda w osobnej linii) gotowe
+#
+# ZASADY:
+# - Ocena może być od 2 do 5 (włącznie).
+# - Jeśli użytkownik poda ocenę spoza zakresu (np. 1 albo 6),
+#     → program natychmiast kończy wczytywanie
+#     → i liczy statystyki tylko z poprawnych ocen.
+#
+# PROGRAM MA WYPISAĆ:
+#   1) Ile poprawnych ocen wczytano.
+#   2) Ile było ocen niedostatecznych (2).
+#   3) Ile było "dobrych" ocen: 4 lub 5.
+#   4) Średnią arytmetyczną poprawnych ocen.
+#
+# Przykład:
+#     Wejście:
+#         5
+#         3
+#         4
+#         5
+#         1
+#
+#     Wynik:
+#         Wczytano 3 poprawnych ocen
+#         Niedostatecznych: 0
+#         Dobrych (4 lub 5): 2
+#         Średnia: 4.0
+#
+# ==========================================
+
+ilosc_ocen = int(input("Ile ocen chcesz podać?: "))
+
+poprawne_oceny = 0
+niedostatecznych = 0
+dobra_ocena = 0
+suma_ocen = 0
+
+for i in range(ilosc_ocen):
+    ocena = int(input(f"Podaj ocenę {i+1}: "))
+    if ocena < 2 or ocena > 5:
+        print("Ocena może być tylko od 2 do 5.")
+        break
+    poprawne_oceny += 1
+    suma_ocen += ocena
+
+    if ocena == 2:
+        niedostatecznych += 1
+    if ocena == 4 or ocena == 5:
+        dobra_ocena += 1
+
+
+print("---PODSUMOWANIE---")
+print(f"Wczytano {poprawne_oceny} poprawnych ocen.")
+print(f"Niedostatecznych {niedostatecznych}")
+print(f"Dobrych (4 lub 5): {dobra_ocena}")
+print(f"Średnia: {suma_ocen / poprawne_oceny}")
---
+++ Zajecie_3_warsztaty/zajecia_3_warsztaty.py
@@ -0,0 +1,31 @@
+print("Dodawania filmow")
+
+ilosc_filmow = int(input("Podaj ile chcesz dodac filmow na serwer?: "))
+
+calkowita_wielkosc_filmow = 0
+maksymalna_pojemnosc_slotu = 10000
+ilosc_slotow = 1
+zajete_miejsce_aktualnego_slotu = 0
+slot_z_najmniejszym_zajetym_miejscem = 1
+najlezejszy_slot = 10000
+
+for numer_filmow in range(ilosc_filmow):
+    rozmiar_filmow = float(input(f"Poda rozmiar filmu {numer_filmow +1}(w MB): "))
+    if rozmiar_filmow < 10 or rozmiar_filmow > 3000:
+        break
+    calkowita_wielkosc_filmow += rozmiar_filmow
+    if zajete_miejsce_aktualnego_slotu + rozmiar_filmow > maksymalna_pojemnosc_slotu:
+        if zajete_miejsce_aktualnego_slotu < najlezejszy_slot:
+            najlezejszy_slot = zajete_miejsce_aktualnego_slotu
+            slot_z_najmniejszym_zajetym_miejscem = ilosc_slotow
+        ilosc_slotow += 1
+        zajete_miejsce_aktualnego_slotu = 0
+    zajete_miejsce_aktualnego_slotu += rozmiar_filmow
+if calkowita_wielkosc_filmow == 0:
+    ilosc_slotow = 0
+    print("Nie dodales zadnego filmu")
+else:
+    print(f"Calkowita rozmiar wszystkich fulmow {calkowita_wielkosc_filmow} Mb")
+    print(f"Ilosc slotów: {ilosc_slotow}")
+    print(f"Ilosc pustej przestszeni: {ilosc_slotow * maksymalna_pojemnosc_slotu - calkowita_wielkosc_filmow}")
+    print(f"Najlzejszy slot to {slot_z_najmniejszym_zajetym_miejscem}, mial {maksymalna_pojemnosc_slotu - najlezejszy_slot} wolnego miejsca")
